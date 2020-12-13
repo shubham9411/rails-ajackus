ebd084d90dbd755aabbebdd80b082f8e144cd10e
Shubham Pandey <shubham9411@gmail.com>
2020-12-13 23:28:10 +0530

    [FEATURE] Translation

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module RailsAjackus
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = %i[en hi]
+    config.i18n.default_locale = :en
   end
 end
