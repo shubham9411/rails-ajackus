365605c4a96d6ace5ed441d610993db2c1d85d62
Shubham Pandey <shubham9411@gmail.com>
2020-12-13 15:45:06 +0530

    [FEATURE] Contact page with vendor design

---
+++ app/models/contact.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+  validates :message, presence: true
+end
