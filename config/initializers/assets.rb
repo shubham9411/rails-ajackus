209afc32dd3952d3b8f6c3e62039a0d344f2b462
Shubham Pandey <shubham9411@gmail.com>
2020-12-13 16:14:41 +0530

    Font awesome gem

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,11 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( contact.js )
+Rails.application.config.assets.precompile += %w( application.js contact.js )
